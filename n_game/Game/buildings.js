4727c060ca39df18e5a091f5a619fe28f0513a69
NoLevelCap - Mac <Capitals@Jamess-MacBook-Air.local>
2017-03-03 11:40:02 +0000

    11:39 Friday 3rd March
    I don’t want to lose progress after disconnect.

--- n_game/Game/buildings.js
+++ n_game/Game/buildings.js
@@ -1,4 +1,5 @@
 var prooms = [];
+var pblocks = [];
 
 function Building(){
   this.content = generateRooms();
@@ -10,13 +11,16 @@ function generateRooms(){
     floor = new Array(10);
     var mx = 0;
     for (var x = 0; x < 10; x++) {
-      prm = prooms["Main"];
+    /*  prm = prooms["Main"];
       if(x % 2 == 0){
           prm = prooms["Long"];
       }
       if(x % 3 == 0 && y > 0){
           prm = prooms["Three_Lower"];
       }
+      floor[x] = new Room(prm, x, y, mx);
+      mx += prm.width;*/
+      prm = prooms["Main"];
       floor[x] = new Room(prm, x, y, mx);
       mx += prm.width;
     }
@@ -26,27 +30,56 @@ function generateRooms(){
 }
 
 function Room(PreRoom, x, y, mx){
-  this.tid = PreRoom.tid;
+  this.room = new Container();
   this.x = x;
   this.y = y;
-  this.Sprite = new Sprite(id[PreRoom.tid + ".png"])
-  this.Sprite.position.set(mx, y*PreRoom.height);
-  bld_container.addChild(this.Sprite);
+  this.mx = mx;
+  this.blocks = PreRoom.blocks;
+  this.preroom = PreRoom;
+  this.roomimage = [];
+  this.initSprites = new function(){
+    for (var room in this.blocks) {
+      if (this.blocks.hasOwnProperty(room)) {
+        console.log(room);
+        //  this.roomimage[room] = new Sprite(id[tid + ".png"])
+      }
+    }
+  };
+
+  this.initSprites();
+  //up, left, right, down
 }
 
-function PreRoom(tid, fs){
+function Block(tid){
   this.tid = tid;
-  this.fs = fs;
-  this.width = fs*24;
+  this.width = 24;
   this.height = 16;
+  this.up = -1;
+  this.down = -1;
+  this.right = -1;
+  this.bottom = -1;
+
+  this.setContainer = new function(container){
+    //container.addChild(this.sprite);
+  };
+}
+
+function PreRoom(blocks){
+  this.blocks = blocks;
 }
 
 function initPreRooms(){
-  prooms["Main"] = new PreRoom("building_0", 1, 1);
-  prooms["Long"] = new PreRoom("building_long_0", 2, 1);
-  prooms["Three_Upper"] = new PreRoom("building_long_1", 2, 1);
-  prooms["Three_Lower"] = new PreRoom("building_long_2", 2, 1);
+  initBlocks();
 
-  prooms["Three_Lower"].upper = prooms["Three_Upper"];
-  prooms["Three_Upper"].lower = prooms["Three_Lower"];
+  prooms["Main"] = new PreRoom([pblocks["single"]]);
+  //prooms["Long"] = new PreRoom("building_long_0", 2, 1);
+  //prooms["Three_Upper"] = new PreRoom("building_long_1", 2, 1);
+  //prooms["Three_Lower"] = new PreRoom("building_long_2", 2, 1);
+
+  //prooms["Three_Lower"].upper = prooms["Three_Upper"];
+  //prooms["Three_Upper"].lower = prooms["Three_Lower"];
+}
+
+function initBlocks(){
+  pblocks["single"] = new Block("building_0");
 }
